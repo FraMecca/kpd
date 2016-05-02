2520a013c9980543a45bd16ea2e2e2e8e14aa34d
PesceWanda <me@francescomecca.eu>
2016-05-02 23:23:02 +0200

    setup.py added subprocess to build cython module, not the cleanest way

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
+import subprocess
 import os
-setup (name='kpd',
-        scripts=['kpd'],
-        py_modules=['shell', 'util', 'config_file', 'MPDdatabase', 'formatter'],
-        packages=find_packages())
+
+os.chdir ("./search")
+subprocess.call (["python3", "setup.py", "build_ext", "--inplace"])
+os.chdir ('..')
+# setup (name='kpd',
+        # scripts=['kpd'],
+        # py_modules=['shell', 'util', 'config_file', 'MPDdatabase', 'formatter'],
+        # packages=find_packages())
