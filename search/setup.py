b5a9cf3c2213c83bff8b8e7cb480b097d527b6c1
PesceWanda <me@francescomecca.eu>
2016-09-20 02:24:24 +0200

    fst clean

--- search/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import  Extension, setup
-from Cython.Build import cythonize
-
-ext = Extension (name = "cython_search", sources = ["list_kpd.c", "C_search.c", "cython_search.pyx", "filter.c"],extra_compile_args=["-O2"], extra_link_args=["-lz"] )
-setup (ext_modules = cythonize (ext))
-
