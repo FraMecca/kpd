8e09ef5146fac6af94103442fa0015e3da33ae87
PesceWanda <me@francescomecca.eu>
2016-09-20 16:06:28 +0200

    primo util.c

---
+++ src/util.c
@@ -0,0 +1,29 @@
+#ifndef KPD_UTIL_H
+#define KPD_UTIL_H
+
+#include <mpd/client.h> // libmpdclient
+#include <gc.h> // garbage collector
+#include <bool.h> // true false
+
+/* start play song
+ * can accept zero or one arguments
+ * if zero args, play current song or song zero
+ * if arg, play the song specified by the number (-1 because mpd start from 0)
+ * prototype is standard because it is called from parse_args
+ */
+bool
+play (struct *mpd_connection, char **args, int n);
+
+struct mpd_connection*
+open_connection (const char *host, unsigned port, unsigned timeout_ms)
+{
+	struct mpd_connection *new_conn;
+	mpd_error conn_err;
+
+	new_conn = mpd_connection_new(host, port, timeout_ms);
+
+}
+
+
+
+#endif
